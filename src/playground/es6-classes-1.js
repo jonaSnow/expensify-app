40bd7c2cac7cdaa427b973fe87859dcf1ae6fd5d
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2018-04-28 20:31:11 -0400

    sub classes excercise

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -4,7 +4,7 @@ class Person {
 		this.age = age;
 	}
 
-	getGretting() {
+	getGreeting() {
 		return `Hi. I am ${this.name}!`;
 	}
 
@@ -13,8 +13,47 @@ class Person {
 	}
 }
 
-const me = new Person('Jona Guzman', 27);
-console.log(me.getDescription());
+class Student extends Person {
+	constructor(name, age, major) {
+		super(name, age);
+		this.major = major;
+	}
 
-const other = new Person();
-console.log(other.getDescription());
+	hasMajor() {
+		return !!this.major;
+	}
+
+	getDescription() {
+		let description = super.getDescription();
+
+		if (this.hasMajor()) {
+			description += ` Their major is ${this.major}.`
+		}
+
+		return description;
+	}
+}
+
+class Traveler extends Person {
+	constructor(name, age, homeLocation) {
+		super(name, age);
+		this.homeLocation = homeLocation;
+	}
+
+	getGreeting() {
+		let greeting = super.getGreeting();
+
+		if (!!this.homeLocation) {
+			greeting += ` I'm visiting from ${this.homeLocation}.`;
+		}
+
+		return greeting;
+	}
+}
+
+
+const me = new Traveler('Jona Guzman', 27, 'Conneticut');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
