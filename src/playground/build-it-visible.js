1e43913d3ced2e7f2337fddb48cc66504f9a09ae
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2018-05-06 17:17:39 -0400

    created VisibilityToggle component

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,65 +1,35 @@
-let visibility = false;
+class VisibilityToggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+		this.state = {
+			visibility: false
+		};
+	}
 
-const toggleVisibility = () => {
-	visibility = !visibility;
-	render()
-};
+	handleToggleVisibility() {
+		this.setState((prevState) => {
+			return {
+				visibility: !prevState.visibility
+			};
+		});
+	}
 
-const appRoot = document.getElementById('app');
+	render() {
+		return (
+			<div>
+				<h1>Visibility Toggle</h1>
+				<button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'hide details' : 'show details'}</button>
+				{this.state.visibility && 
+					(
+						<div>
+							<p>Here are some details now</p>
+						</div>
+					) 
+				}
+			</div>
+		);
+	}
+}
 
-const render = () => {
-	const jsx = (
-		<div>
-			<h1>Visibility Toggle</h1>
-			<button onClick={toggleVisibility}>{visibility ? 'hide details' : 'show details'}</button>
-			{visibility && 
-				(
-					<div>
-						<p>Here are some details now</p>
-					</div>
-				) 
-			}
-		</div>
-	);
-
-	ReactDOM.render(jsx, appRoot);
-};
-
-render();
-
-/*
-pre video review
-const app json var not needed
-use let "variable name" = boolean
-condition for visibility was backwards
-not as a nice as "if visible button is named hide details"
-{app.isVisible ? app.details : null} == {visibility && (<div><p>Here are some details now</p></div>)}
-second way is cleaner since it was only visible return details otherwise return nothing
-""
-const app = {
-	title: 'Visibility Toggle',
-	details: "Here are some details",
-	isVisible: false
-};
-
-const toggleDetails = () => {
-	app.isVisible = !app.isVisible;
-	renderVisibilityToggleApp()
-};
-
-const appRoot = document.getElementById('app');
-
-const renderVisibilityToggleApp = () => {
-	const template = (
-		<div>
-			<h1>{app.title}</h1>
-			<button onClick={toggleDetails}>{!app.isVisible ? 'show details' : 'hide details'}</button>
-			<p>{app.isVisible ? app.details : null}</p>
-		</div>
-	);
-
-	ReactDOM.render(template, appRoot);
-};
-
-renderVisibilityToggleApp();
-*/
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
