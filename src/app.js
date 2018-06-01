9c653f9927b11fdb5d9fcbeab2abe12a23b04e29
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2018-05-31 22:25:50 -0400

    split components into own files/webpack set up

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-// // import './utils.js';
-// import { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(45, 35));
-
-// import isAdult and canDrink
-// use both printing result to the console
-
-import { isAdult, canDrink } from './person.js';
-console.log(isAdult(17));
-console.log(canDrink(27));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- src/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('utils.js running');
-
-export const square = (x) => x * x;
-
-export const add = (a, b) => a + b;
-
-// export { square, add };
-
-// exports - default export - named exports
