caf285f47468dd7845b21fd9a0eda48f87874874
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2018-06-28 22:28:48 -0400

    styling react modal

--- src/app.js
+++ src/app.js
@@ -4,49 +4,4 @@ import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const Layout = (props) => {
-  return (
-    <div>
-      <p>header</p>
-      {props.children}
-      <p>footer</p>
-    </div>
-  );
-};
-
 ReactDOM.render((<IndecisionApp />), document.getElementById('app'));
-
-// ReactDOM.render((
-// 	<Layout>
-// 		<div>
-// 			<h1>Page Title</h1>
-// 			<p>This is my page</p>
-// 		</div>
-// 	</Layout>
-// ), document.getElementById('app'));
-
-// class OldSyntax {
-// 	constructor() {
-// 		this.name = 'Jona';
-// 	}
-
-// 	getGreeting() {
-// 		return `Hi. My name is ${this.name}.`;
-// 	}
-// }
-
-// const oldSyntax = new OldSyntax();
-// console.log(oldSyntax.getGreeting());
-
-// // -----------------------
-
-// class NewSyntax {
-// 	name = 'Jen';
-// 	getGreeting = () => {
-// 		return `Hi. My name is ${this.name}.`;
-// 	}
-// }
-
-// const newSyntax = new NewSyntax();
-// const newGetgreeting = newSyntax.getGreeting;
-// console.log(newGetgreeting());
