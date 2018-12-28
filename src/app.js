aba7f60a9a6da05ca8091c6d5bacc27c519cf672
Jonathan Guzman <guzman.jonathan.r@gmail.com>
2018-12-27 22:30:40 -0500

    redux in expensify app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+// Redux
+const store = configureStore();
+store.subscribe(() => {
+  const state = store.getState();
+});
+
+store.dispatch(addExpense({ description: "Water Bill" }));
+store.dispatch(addExpense({ description: "Gas Bill" }));
+store.dispatch(setTextFilter('Water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/reducers/expenses.js
@@ -0,0 +1,26 @@
+// Expenses Reducer
+
+const expensesReducerDefaultState = [];
+
+export default (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      // do not need spread operator for a filter bc it doesnt replace array
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        }
+      }) 
+    default:
+      return state;
+  }
+};
