73d9c08b2ce475815f26c712c2832826ef006497
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2019-06-01 19:30:12 -0400

    added link to portfolio page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Header = () => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact>Home</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    <NavLink to="/" activeClassName="is-active" exact>Home{"\t"}</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense{"\t"}</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help{"\t"}</NavLink>
   </header>
 );
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -10,6 +10,7 @@ import NotFoundPage from '../components/NotFoundPage';
 const AppRouter = () => (
 <BrowserRouter>
     <div>
+      <a style={{textDecoration: "none", color: "blue"}} href="http://jonathan-guzman.s3-website-us-east-1.amazonaws.com">Back To Jona's Portfolio</a>
       <Header />
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
